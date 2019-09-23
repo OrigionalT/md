f8304c29c66f1a05f1e96390704345b9217b1a99
wCap <ywmirai@163.com>
2019-09-23 09:50:49 +0800

    005_导入areas.sql文件

--- meiduo_mall/meiduo_mall/settings/dev.py
+++ meiduo_mall/meiduo_mall/settings/dev.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'users.apps.UsersConfig',
     'oauth.apps.OauthConfig',
+    'areas.apps.AreasConfig',
 ]
 
 MIDDLEWARE = [
---
+++ meiduo_mall/scripts/import_areas_data_to_db.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+
+mysql -h127.0.0.1 -umeiduo -pmeiduo md_mall < areas.sql
